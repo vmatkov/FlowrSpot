1008a178d6af3d2b55c00f24ead1f6b39ce385b9
Matjaž <vmatkov@gmail.com>
2018-08-26 21:14:13 +0200

    App Icon Changed

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, ToolbarAndroid, View} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,11 +21,21 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+        <ToolbarAndroid
+        //logo={require('./app_logo.png')}
+        title="AwesomeApp"
+        actions={[{title: 'Settings', /*icon: require('./icon_settings.png'),*/ show: 'always'}]}
+        onActionSelected={this.onActionSelected} />
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
+  }
+  onActionSelected(position) {
+    if (position === 0) { // index of 'Settings'
+      showSettings();
+    }
   }
 }
 
