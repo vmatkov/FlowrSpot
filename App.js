b809cb8f0993a2369ff68b56152f48aeb7580622
Matjaž <vmatkov@gmail.com>
2018-09-06 10:48:37 +0200

    Fetch from Flowers API

--- App.js
+++ App.js
@@ -14,7 +14,8 @@ import {
   StatusBar,
   ToolbarAndroid,
   ImageBackground,
-  View
+  View,
+  ActivityIndicator,
 } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 
@@ -27,45 +28,87 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <ImageBackground
-        source={require('./Images/pl_hero_2018-08-30/drawable-mdpi/pl_hero.png')}
-        style={styles.container}>
-        <View style={styles.view_container}>
-          <StatusBar backgroundColor="#e09186" animated={true} />
-          <ToolbarAndroid
-              style={styles.toolbar}
-              title="FlowrSpot"
-              navIcon={require("./android/app/src/main/res/mipmap-hdpi/ic_launcher_round.png")}
-              onActionSelected={this.onActionSelected}
-              titleColor= "#e09186"
-              actions = {[
-                {title: "About", show: "never"}
-              ]}
-              />
-          <Text style={styles.welcome}>Discover flowers around you</Text>
-          <Text style={styles.instructions}>Explore between more than 8.427 sightings</Text>
-          <SearchBar
-            onChangeText={someMethod}
-            onClearText={someMethod}
-            placeholder='Looking for something specific?' />
-        </View>
-      </ImageBackground>
-    );
-  }
-  onActionSelected(position) {
-    if (position === 0) { // index of 'Settings'
-      showSettings();
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoading: true,
+      dataSource: null,
     }
   }
+
+  componentDidMount(){
+
+    let req = new Request('http://flowrspot-api.herokuapp.com/api/v1/flowers');
+
+    return fetch(req)
+      .then( (response) => response.json() )
+      .then( (responseJson) => {
+          this.setState({
+            isLoading: false,
+            dataSource: responseJson.flowers
+          })
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+  }
+
+
+  render() {
+
+    if(this.state.isLoading)
+    {
+      return(
+        <View style={styles.container}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+    else
+    {
+      return (
+        <ImageBackground
+          source={require('./Images/pl_hero_2018-08-30/drawable-mdpi/pl_hero.png')}
+          style={styles.container}>
+          <View style={styles.view_container}>
+            <StatusBar backgroundColor="#e09186" animated={true} />
+            <ToolbarAndroid
+                style={styles.toolbar}
+                title="FlowrSpot"
+                navIcon={require("./Images/flowr.png")}
+                //onActionSelected={this.onActionSelected}
+                titleColor= "#e09186"
+                actions = {[
+                  {title: "About", show: "never"}
+                ]}
+                />
+            <Text style={styles.welcome}>Discover flowers around you</Text>
+            <Text style={styles.instructions}>Explore between more than 8.427 sightings</Text>
+            <SearchBar
+                round
+                style={styles.searchbar}              
+                icon={{ type: 'FontAwesome', name: 'search' }}
+                placeholder='Looking for something specific?' />
+          </View>
+        </ImageBackground>
+      );
+    }
+  }
+
+//  onActionSelected(position) {
+//    if (position === 0) { // index of 'Settings'
+//      showSettings();
+//    }
+//  }
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    height: 315,
-    width: 360,
+    height: '60%',
+    width: '100%',
     justifyContent: 'center'
     //alignItems: 'stretch',
     //backgroundColor: '#F5FCFF',
@@ -94,4 +137,10 @@ const styles = StyleSheet.create({
     height: 56,
     alignSelf: 'stretch'
   },
+  searchbar: {
+    backgroundColor: '#ffffff',
+    margin: 20
+  }
 });
+
+ // adb shell input keyevent 82
