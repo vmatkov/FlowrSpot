aeb44906f0b039ff81de9e4a55e12aec45ca262b
Matjaž <vmatkov@gmail.com>
2018-09-13 19:36:13 +0200

    Created mocks repo

--- __test__/App.test.js
+++ __test__/App.test.js
@@ -1,11 +1,34 @@
 import 'react-native';
 import React, {Component} from 'react';
 import App from '../App';
+//import 'isomorphic-fetch';
 
-import render from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 
-it('renders correctly', () =>{
-    const hello = render.create(
+it('Renders correctly', () => {
+    const app = renderer.create(
         <App />
     )
+})
+
+it('Searching for flower', async () => {
+    let AppData = renderer.create(<App />).getInstance();
+
+    global.fetch = jest.fn().mockImplementation( () => {
+        var p = new Promise((resolve, reject) => {
+            resolve({
+                json: () => {
+                    return { id: 1, name : 'Alpski volcin', latin_name: 'Daphne alpina'}
+                }
+            })
+        })
+    });
+
+    const response = await AppData.componentDidMount();
+    console.log(AppData.state);
+    //console.log(AppData)
+    let text = "Alpski";
+    AppData.onTextChange(text);
+    console.log(AppData.state);
+    
 })
